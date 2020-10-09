1af2d450878b43267bf01724c2e83d88d55e32ff
Attila <59293104+sweeptic@users.noreply.github.com>
2020-10-09 12:14:52 +0200

    524. creating webserver  with node

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
-const text = "this is a test";
+// const text = "this is a test";
 
-fs.writeFile('node-message.txt', text).then(() => {
-   console.log('wrote file');
+// fs.writeFile('node-message.txt', text).then(() => {
+//    console.log('wrote file');
+// })
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+   res.end('hello world from node');
 })
+
+server.listen(3000);
